bda5304a554953d4418c44600daab12059323495
Mark Matlock <techmatlock@gmail.com>
2024-05-13 18:34:24 +0000

    Completed typescript-primitives-and-variables exercise

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+'use strict';
+const fullName = 'Mark Matlock';
+console.log('value of fullName:', fullName);
+const isCool = true;
+console.log('value of isCool:', isCool);
+const totalPets = 6;
+console.log('value of totalPets:', totalPets);
+// Typeof operator
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+// Undefined and null data types
+const empty = null;
+console.log('value of empty:', empty);
+console.log('value of empty:', typeof empty);
+const nothing = undefined;
+console.log('value of nothing', nothing);
+console.log('typeof nothing', typeof nothing);
