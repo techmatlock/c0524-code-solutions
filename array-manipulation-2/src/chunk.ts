ede0fb96c435b90dd682710f5679d78cd906a3d9
Mark Matlock <techmatlock@gmail.com>
2024-05-20 21:31:41 +0000

    storing chunk.ts

--- array-manipulation-2/src/chunk.ts
+++ array-manipulation-2/src/chunk.ts
@@ -1,6 +1,6 @@
 /* exported chunk */
 function chunk(array: unknown[], size: number): unknown[] {
-  const chunks = [];
+  const chunks = new Array(size);
   console.log(size);
   for (let i = 0; i < size; i++) {
     // array[0] array[1] ; array[2] array[3]
