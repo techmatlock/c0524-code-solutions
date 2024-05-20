a9ddadea9b00d190fb24945394af41fc3d719d69
Mark Matlock <techmatlock@gmail.com>
2024-05-20 16:14:08 +0000

    second commit

--- array-manipulation-2/src/chunk.ts
+++ array-manipulation-2/src/chunk.ts
@@ -2,6 +2,7 @@
 function chunk(array: unknown[], size: number): unknown {
   const newArr = [];
   let count = 0;
+
   while (count < array.length) {
     for (let i = 0; i < size; i++) {
       newArr.push([array[i]]);
