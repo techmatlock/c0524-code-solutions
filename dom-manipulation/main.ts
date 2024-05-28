f021a26f90c940be4468040972e09672ef420add
Mark Matlock <techmatlock@gmail.com>
2024-05-28 21:44:26 +0000

    Completed dom-manipulation exercise

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,30 @@
+let numOfClicks = 0;
+
+const $hotBtn = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+if (!$hotBtn) throw new Error('The $hotBtn query failed.');
+
+function increaseCount(): void {
+  if (!$hotBtn || !$clickCount) {
+    throw new Error('The $hotButton or $clickCount query failed');
+  }
+  numOfClicks += 1;
+  $clickCount.textContent = `Clicks: ${numOfClicks}`;
+
+  if (numOfClicks < 4) {
+    $hotBtn.className = 'hot-button cold';
+  } else if (numOfClicks < 7) {
+    $hotBtn.className = 'hot-button cool';
+  } else if (numOfClicks < 10) {
+    $hotBtn.className = 'hot-button tepid';
+  } else if (numOfClicks < 13) {
+    $hotBtn.className = 'hot-button warm';
+  } else if (numOfClicks < 16) {
+    $hotBtn.className = 'hot-button hot';
+  } else {
+    $hotBtn.className = 'hot-button nuclear';
+  }
+}
+
+$hotBtn.addEventListener('click', increaseCount);
