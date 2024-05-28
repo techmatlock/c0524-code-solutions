565f07969fd3857bc461e57563f305fcc3774abc
Mark Matlock <techmatlock@gmail.com>
2024-05-28 19:02:04 +0000

    Completed dom-events exercise

---
+++ dom-events/buttons.js
@@ -0,0 +1,26 @@
+'use strict';
+const clickBtn = document.querySelector('.click-button'); // returns a button element object
+if (!clickBtn) throw new Error('clickBtn not found.');
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('event', event);
+  console.log('event.target', event.target);
+}
+clickBtn.addEventListener('click', handleClick);
+// Mouseover Event
+const hoverBtn = document.querySelector('.hover-button');
+if (!hoverBtn) throw new Error('hoverBtn not found');
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log('event', event);
+  console.log('event.target property', event.target);
+}
+hoverBtn.addEventListener('mouseover', handleMouseover); // mouseover is an event listener on the button element object
+const dblClickBtn = document.querySelector('.double-click-button');
+if (!dblClickBtn) throw new Error('dblClickBtn not found.');
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event', event);
+  console.log('event.target', event.target);
+}
+dblClickBtn.addEventListener('dblclick', handleDoubleClick);
