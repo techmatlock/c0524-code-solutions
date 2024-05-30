ebfcc99a69c3b2258e58d8905fc9d10b99084540
Mark Matlock <techmatlock@gmail.com>
2024-05-30 21:14:56 +0000

    Completed typescript-forms exercise

---
+++ typescript-forms/form-values.ts
@@ -0,0 +1,26 @@
+interface FormElements extends HTMLFormControlsCollection {
+  name: HTMLInputElement;
+  email: HTMLInputElement;
+  message: HTMLTextAreaElement;
+}
+
+const $form = document.querySelector('#contact-form') as HTMLFormElement;
+
+function handleSubmit(event: Event): void {
+  event.preventDefault();
+
+  // get elements property of the object that stores the DOM reference to the form
+  const $formElements = $form.elements as FormElements;
+
+  const $inputName = $formElements.name.value;
+  const $inputEmail = $formElements.email.value;
+  const $message = $formElements.message.value;
+
+  const result = { name: $inputName, email: $inputEmail, message: $message };
+
+  console.log(result);
+
+  $form.reset();
+}
+
+$form.addEventListener('submit', handleSubmit);
