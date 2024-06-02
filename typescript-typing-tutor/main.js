d0bbcae0fd1fb9f9964ad163f2e5b1584b55fb84
Mark Matlock <techmatlock@gmail.com>
2024-06-02 23:59:37 +0000

    Added modal

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,59 +1,62 @@
 'use strict';
 /*
-1. Query DOM for all <span> elements
-2. Create an event listener on the document when user triggers keydown event
-3. Increase index of $spanElements array every time user creates keydown event
-4. Create conditional to check if event.key is equal to $spanElements textcontent
-5. If correct key entered:
-    * Remove bottom border from $spanElements[currentIndex]
-    * Add bottom border to $spanElements[currentIndex + 1]
-    * Change css color to green for $spanElements[currentIndex]
-   If wrong key entered:
-    * Change css color to red for $spanElements[currentIndex]
-6. Check to see if currentIndex is greater than $spanElements.length. If so, game over.
-7. Calculate accuracy by taking the sum of correct guesses divided by the total length of $spanElements
-
-Adding a modal
-1. Create the variables for the modal window, the yes button, and the no button that reference the DOM elements.
-   * If user selects yes to play again, reset currentIndex, accuracy, count, guessedCorrect
-   * If no, close modal and don't reset any variables.
-2. Add event listeners for the yes and no button.
-3. Add the CSS for the modal window and add the dialog element in the HTML.  Add accuracy score to modal window.
+1. Add event listener for buttons on parent container (modal-buttons)
+2. Use event target to get the closest element
+3. Save reference to element in a variable
 */
 const $spanElements = document.querySelectorAll('span');
 const $modal = document.querySelector('dialog');
+const $accuracyScore = document.querySelector('.accuracy-score');
+const $modalBtns = document.querySelector('.modal-buttons');
+if (!$modal) throw new Error('$modal does not exist');
+if (!$accuracyScore) throw new Error('$accuracyScore does not exist');
 let currentIndex = 0;
 let count = 0;
 let guessedCorrect = 0;
 function calculateAccuracy() {
   const accuracy = (guessedCorrect / count) * 100;
-  console.log(accuracy);
-  if (!$modal) throw new Error('$modal does not exist');
+  $accuracyScore?.insertAdjacentHTML('afterend', accuracy.toString());
+}
+function resetGame() {
+  $modal?.classList.add('hidden');
+  currentIndex = 0;
+  count = 0;
+  guessedCorrect = 0;
+}
+function showModal() {
   $modal?.showModal();
+  if (!$modalBtns) throw new Error('$modalBtns does not exist');
+  $modalBtns.addEventListener('click', (event) => {
+    const eventTarget = event.target; // the element on which the event was fired
+    const btnClicked = eventTarget.closest('button');
+    if (!btnClicked) throw new Error('No button clicked');
+    if (btnClicked.className === 'modal-no') {
+      $modal?.classList.add('hidden');
+    } else {
+      resetGame();
+    }
+  });
 }
 function trackCharacter(event) {
   // Track every time key entered even if wrong.
   count++;
   if (event.key === $spanElements[currentIndex].textContent) {
     guessedCorrect++;
-    // Remove any styling if previously wrong key entered
+    // Remove any styling
     $spanElements[currentIndex].classList.remove('red-color');
     $spanElements[currentIndex].classList.remove('border__bottom-red');
-    // Add styling if correct key entered
     $spanElements[currentIndex].classList.remove('border-bottom');
+    // Add styling
     $spanElements[currentIndex].classList.add('green-color');
-    $spanElements[currentIndex + 1].classList.add('border-bottom');
     currentIndex++;
-    if (currentIndex >= $spanElements.length - 1) {
-      calculateAccuracy();
-    }
   } else {
     $spanElements[currentIndex].classList.add('red-color');
     $spanElements[currentIndex].classList.add('border__bottom-red');
   }
-}
-// Apply bottom border to first index when the game first starts
-if (currentIndex === 0) {
-  $spanElements[currentIndex].className = 'border-bottom';
+  if (!$spanElements[currentIndex]) {
+    calculateAccuracy();
+    showModal();
+  }
+  $spanElements[currentIndex].classList.add('border-bottom');
 }
 document.addEventListener('keydown', trackCharacter);
