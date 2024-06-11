b564292a7f1a798754c80bff88e602ff0b9873e1
Mark Matlock <techmatlock@gmail.com>
2024-06-11 16:06:56 +0000

    Working on dot carousel event listener

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -1,46 +1,82 @@
-"use strict";
+'use strict';
 const $leftNavigator = document.querySelector('.left-navigation');
 const $rightNavigator = document.querySelector('.right-navigation');
-const $newImage = document.querySelector('.new-img');
-if (!$leftNavigator)
-    throw new Error('No $leftNavigator found.');
-if (!$rightNavigator)
-    throw new Error('No $rightNavigator found.');
-if (!$newImage)
-    throw new Error('No $newImg found.');
-const images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+const $currentImage = document.querySelector('.new-img');
+const $allDots = document.querySelectorAll('.dot');
+const $dotsContainer = document.querySelector('.dots');
+if (!$currentImage) throw new Error('No $currentImage found.');
+if (!$leftNavigator) throw new Error('No $leftNavigator found.');
+if (!$rightNavigator) throw new Error('No $rightNavigator found.');
+if (!$allDots) throw new Error('No $allDots found.');
+if (!$allDots) throw new Error('No $dotsContainer found.');
+const images = [
+  'images/001.png',
+  'images/004.png',
+  'images/007.png',
+  'images/025.png',
+  'images/039.png',
+];
+let count = 0;
+const intervalID = setInterval(nextImage, 3000);
+const timeoutID = setTimeout(() => {
+  nextImage();
+}, 3000);
 $leftNavigator.addEventListener('click', () => {
-    clearInterval(intervalID);
-    const $currentImg = $newImage.getAttribute('src');
-    if (!$currentImg)
-        throw new Error('No $currentImg found.');
-    images.forEach((img) => {
-        if (img.includes($currentImg)) {
-            const newIndex = img.indexOf($currentImg);
-            $newImage.src = images[newIndex];
-            count = newIndex;
-            setInterval(nextImage);
-        }
-    });
+  clearInterval(intervalID);
+  clearTimeout(timeoutID);
+  const $currentImage = document.querySelector('.new-img');
+  if (!$currentImage) throw new Error('No $newImg found.');
+  const $currentImg = $currentImage.getAttribute('src');
+  if (!$currentImg) throw new Error('No $currentImg found.');
+  images.forEach((img) => {
+    if (img.includes($currentImg)) {
+      const newIndex = images.indexOf($currentImg);
+      if (newIndex === 0) {
+        $currentImage.src = images[images.length - 1];
+      } else {
+        $currentImage.src = images[newIndex - 1];
+      }
+      setInterval(nextImage, 3000);
+    }
+  });
 });
 $rightNavigator.addEventListener('click', () => {
-    clearInterval(intervalID);
-    const $currentImage = $newImage.getAttribute('src');
-    if (!$currentImage)
-        throw new Error('No $currentImage found');
-    images.forEach((img) => {
-        if (img.includes($currentImage)) {
-            const newIndex = img.indexOf($currentImage);
-            $newImage.src = images[newIndex + 1];
-        }
-    });
-});
-let count = 0;
-function nextImage() {
-    if (count > images.length - 1) {
-        count = 0;
+  clearInterval(intervalID);
+  clearTimeout(timeoutID);
+  const $currentImage = document.querySelector('.new-img');
+  if (!$currentImage) throw new Error('No $newImg found.');
+  const $currentImg = $currentImage.getAttribute('src');
+  if (!$currentImg) throw new Error('No $currentImg found.');
+  images.forEach((img) => {
+    if (img.includes($currentImg)) {
+      const newIndex = img.indexOf($currentImg);
+      count = newIndex + 1;
+      $currentImage.src = images[count];
+      setInterval(nextImage, 3000);
     }
-    $newImage.src = images[count];
-    count++;
+  });
+});
+$dotsContainer.addEventListener('click', (event) => {
+  clearInterval(intervalID);
+  clearTimeout(timeoutID);
+  const $eventTarget = event.target;
+  if (!$eventTarget) throw new Error('No $eventTarget found');
+  if ($eventTarget.matches('.dot')) {
+    const itemId = $eventTarget.getAttribute('data-item-id');
+    $currentImage.src = images[itemId];
+    setInterval(nextImage, 3000);
+  }
+});
+function nextImage() {
+  if (count > images.length - 1) {
+    count = 0;
+  }
+  $allDots.forEach((dot) => {
+    dot.classList.remove('solid');
+    if (dot.getAttribute('data-item-id') === count.toString()) {
+      dot.classList.add('solid');
+    }
+  });
+  $currentImage.src = images[count];
+  count++;
 }
-const intervalID = setInterval(nextImage, 1000);
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -1,48 +1,105 @@
-const $leftNavigator = document.querySelector('.left-navigation') as HTMLElement;
-const $rightNavigator = document.querySelector('.right-navigation') as HTMLElement;
-const $newImage = document.querySelector('.new-img') as HTMLImageElement;
+const $leftNavigator = document.querySelector(
+  '.left-navigation'
+) as HTMLElement;
+const $rightNavigator = document.querySelector(
+  '.right-navigation'
+) as HTMLElement;
+const $currentImage = document.querySelector('.new-img') as HTMLImageElement;
+const $allDots = document.querySelectorAll('.dot');
+const $dotsContainer = document.querySelector('.dots') as HTMLElement;
 
+if (!$currentImage) throw new Error('No $currentImage found.');
 if (!$leftNavigator) throw new Error('No $leftNavigator found.');
 if (!$rightNavigator) throw new Error('No $rightNavigator found.');
-if (!$newImage) throw new Error('No $newImg found.');
+if (!$allDots) throw new Error('No $allDots found.');
+if (!$allDots) throw new Error('No $dotsContainer found.');
 
-const images: string[] = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+const images: string[] = [
+  'images/001.png',
+  'images/004.png',
+  'images/007.png',
+  'images/025.png',
+  'images/039.png',
+];
+
+let count: number = 0;
+
+const intervalID = setInterval(nextImage, 3000);
+
+const timeoutID = setTimeout(() => {
+  nextImage();
+}, 3000);
 
 $leftNavigator.addEventListener('click', (): void => {
   clearInterval(intervalID);
-  const $currentImg = $newImage.getAttribute('src');
+  clearTimeout(timeoutID);
+
+  const $currentImage = document.querySelector('.new-img') as HTMLImageElement;
+  if (!$currentImage) throw new Error('No $newImg found.');
+
+  const $currentImg = $currentImage.getAttribute('src');
   if (!$currentImg) throw new Error('No $currentImg found.');
+
   images.forEach((img) => {
-    debugger;
     if (img.includes($currentImg)) {
-      const newIndex = img.indexOf($currentImg)
-      $newImage.src = images[newIndex];
-      count = newIndex;
-      setInterval(nextImage, 1000);
+      const newIndex = images.indexOf($currentImg);
+      if (newIndex === 0) {
+        $currentImage.src = images[images.length - 1];
+      } else {
+        $currentImage.src = images[newIndex - 1];
+      }
+      setInterval(nextImage, 3000);
     }
-  })
+  });
 });
 
 $rightNavigator.addEventListener('click', (): void => {
   clearInterval(intervalID);
-  const $currentImage = $newImage.getAttribute('src');
-  if (!$currentImage) throw new Error('No $currentImage found')
+  clearTimeout(timeoutID);
+
+  const $currentImage = document.querySelector('.new-img') as HTMLImageElement;
+  if (!$currentImage) throw new Error('No $newImg found.');
+
+  const $currentImg = $currentImage.getAttribute('src');
+  if (!$currentImg) throw new Error('No $currentImg found.');
+
   images.forEach((img) => {
-    if (img.includes($currentImage)) {
-      const newIndex = img.indexOf($currentImage);
-      $newImage.src = images[newIndex + 1];
+    if (img.includes($currentImg)) {
+      const newIndex = img.indexOf($currentImg);
+      count = newIndex + 1;
+      $currentImage.src = images[count];
+      setInterval(nextImage, 3000);
     }
-  })
+  });
 });
 
-let count: number = 0;
+$dotsContainer.addEventListener('click', (event: Event): void => {
+  clearInterval(intervalID);
+  clearTimeout(timeoutID);
+
+  const $eventTarget = event.target as HTMLElement;
+  if (!$eventTarget) throw new Error('No $eventTarget found');
+
+  if ($eventTarget.matches('.dot')) {
+    const itemId = $eventTarget.getAttribute('data-item-id');
+    $currentImage.src = images[itemId];
+
+    setInterval(nextImage, 3000);
+  }
+});
+
 function nextImage(): void {
   if (count > images.length - 1) {
     count = 0;
   }
 
-  $newImage.src = images[count];
+  $allDots.forEach((dot) => {
+    dot.classList.remove('solid');
+    if (dot.getAttribute('data-item-id') === count.toString()) {
+      dot.classList.add('solid');
+    }
+  });
+
+  $currentImage.src = images[count];
   count++;
 }
-
-const intervalID = setInterval(nextImage, 1000);
