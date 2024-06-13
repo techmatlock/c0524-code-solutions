9482fbc52c668ba3d00e5a1ec288c2c735bc9218
Mark Matlock <techmatlock@gmail.com>
2024-06-13 22:42:28 +0000

    Completed fetch exercise

---
+++ fetch/main.ts
@@ -0,0 +1,17 @@
+async function getData(): Promise<void> {
+  try {
+    // the fetch method is being called with one argument, and we are awaiting the promise
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    // we have to validate response because fetch will not reject status codes 404 or 500
+    if (!response.ok) throw new Error('Failed to get a response');
+
+    // await the parsing of the response body as JSON
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+getData();
