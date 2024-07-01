a3dab79d180b2ca7a47716815fa01feed1deb46b
Mark Matlock <techmatlock@gmail.com>
2024-07-01 20:57:17 +0000

    Completed typescript-modules exercise

---
+++ typescript-modules/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Body } from './Body';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
