95d7849daafb87f61343c49fccde9f862f696697
Mark Matlock <techmatlock@gmail.com>
2024-07-03 23:54:31 +0000

    Modified Tied state value and class logic for Toggle component

--- react-toggle-switch/my-app/src/Toggle.tsx
+++ react-toggle-switch/my-app/src/Toggle.tsx
@@ -2,26 +2,30 @@ import './Toggle.css';
 import { useState } from 'react';
 
 export function Toggle() {
-  const [toggle, setToggle] = useState('off');
+  const [toggle, setToggle] = useState(false);
 
   function handleToggle() {
-    if (toggle === 'off') {
-      setToggle('on');
+    if (toggle === false) {
+      setToggle(true);
     } else {
-      setToggle('off');
+      setToggle(false);
     }
   }
+
+  const toggleClass = toggle ? 'on' : 'off';
 
   return (
     <>
       <div className="row">
         <div className="column-half justify-center align-center">
-          <div onClick={handleToggle} className={`toggle-switch ${toggle}`}>
-            <div className={`switch ${toggle}`}></div>
+          <div
+            onClick={handleToggle}
+            className={`toggle-switch ${toggleClass}`}>
+            <div className={`switch ${toggleClass}`}></div>
           </div>
         </div>
         <div className="column-half justify-center align-center left-margin">
-          <p className="label">{toggle === 'off' ? 'Off' : 'On'}</p>
+          <p className="label">{toggle ? 'On' : 'Off'}</p>
         </div>
       </div>
     </>
