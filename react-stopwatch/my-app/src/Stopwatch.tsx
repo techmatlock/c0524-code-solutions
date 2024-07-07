124cd60a0e639743bcde179b074051ac569990f0
Mark Matlock <techmatlock@gmail.com>
2024-07-07 23:48:14 +0000

    Added handlePause and handleFace event handlers

--- react-stopwatch/my-app/src/Stopwatch.tsx
+++ react-stopwatch/my-app/src/Stopwatch.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { FaPlay } from 'react-icons/fa';
 import { FaPause } from 'react-icons/fa6';
 
 let playToggle = '';
 let pauseToggle = 'hidden';
-export function Stopwatch () {
+export function Stopwatch() {
   const [count, setCount] = useState(0);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
 
@@ -13,18 +13,35 @@ export function Stopwatch () {
     pauseToggle = '';
     const intervalId = setInterval(() => {
       setCount((prev) => prev + 1);
-    }, 1000)
+    }, 1000);
     setIntervalId(intervalId);
+  };
+
+  const handlePause = () => {
+    playToggle = '';
+    pauseToggle = 'hidden';
+    clearInterval(intervalId);
+  };
+
+  function handleFace() {
+    if (pauseToggle === '') {
+      clearInterval(intervalId);
+      setCount(0);
+      playToggle = '';
+      pauseToggle = 'hidden';
+    }
   }
 
   return (
     <>
-      <div className="flex items-center justify-center circle w-72 h-72 border-2 border-purple-800 rounded-full">
+      <div
+        onClick={handleFace}
+        className="flex items-center justify-center circle w-72 h-72 border-2 border-purple-800 rounded-full">
         <div className="count text-2xl">{count}</div>
       </div>
       <div className="flex justify-center mt-4 text-2xl">
         <FaPlay onClick={handlePlay} className={`${playToggle}`} />
-        <FaPause className={`${pauseToggle}`} />
+        <FaPause onClick={handlePause} className={`${pauseToggle}`} />
       </div>
     </>
   );
