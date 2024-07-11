91738e320cc7947f1344e29507d2e55976e28328
Mark Matlock <techmatlock@gmail.com>
2024-07-11 15:42:48 +0000

    Removed fragment from App component

--- react-multiple-pages/my-app/src/App.tsx
+++ react-multiple-pages/my-app/src/App.tsx
@@ -6,17 +6,14 @@ import { NotFound } from './pages/NotFound';
 import { Details } from './pages/Details';
 
 export function App() {
-
   return (
-    <>
     <Routes>
       <Route path="/" element={<Header />}>
-      <Route index element={<Dashboard />} />
-      <Route path="details/:itemId" element={<Details />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound  />} />
+        <Route index element={<Dashboard />} />
+        <Route path="details/:itemId" element={<Details />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-    </>
   );
 }
