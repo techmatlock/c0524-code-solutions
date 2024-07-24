0f35fec2c74d0b1188ae43563cd70b9a1a919598
Mark Matlock <techmatlock@gmail.com>
2024-07-24 23:27:06 +0000

    Completed full-stack-practice

---
+++ full-stack-practice/client/src/pages/Details.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import { type Product } from '../lib';
+import { toDollars } from '../lib';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+export function Details() {
+  const [product, setProduct] = useState<Product>();
+  const [error, setError] = useState<unknown>();
+
+  const { productId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadItems() {
+      try {
+        const response = await fetch(`/api/products/${productId}`);
+        if (!response.ok)
+          throw new Error(`Response status: ${response.status}`);
+        const product = (await response.json()) as Product;
+        setProduct(product);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    loadItems();
+  }, [productId]);
+
+  if (error || !product) {
+    return (
+      <div className="flex flex-wrap justify-center items-center mt-2">
+        Error! {error instanceof Error ? error.message : 'Unknown error'}
+        <div className="flex justify-center w-full">
+          <Link to="/">&lt; Back to Catalog</Link>
+        </div>
+      </div>
+    );
+  }
+
+  function handleClick(name: string) {
+    alert(`Added ${name} to cart.`);
+    navigate('/');
+  }
+
+  return (
+    <>
+      <div className="flex flex-wrap mx-6 p-8 border-2 border-gray-400 rounded-md">
+        <div className="basis-full px-4 mb-4">
+          <Link to="/">&lt; Back to Catalog</Link>
+        </div>
+        <div className="flex basis-full">
+          <div className="flex">
+            <div className="basis-half max-w-lg">
+              <img
+                className="w-full object-cover mb-4 px-4"
+                src={`${product.imageUrl}`}
+                alt={`A picture of ${product.name}`}
+              />
+            </div>
+          </div>
+          <div className="flex px-10">
+            <div className="basis-half">
+              <h1 className="text-3xl">{product.name}</h1>
+              <h2 className="pt-4 text-xl">{toDollars(product.price)}</h2>
+              <p className="pt-4">{product.shortDescription}</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="basis-full px-4">
+            <p>{product.longDescription}</p>
+          </div>
+        </div>
+      </div>
+      <div className="mx-6 px-10">
+        <button
+          onClick={() => handleClick(product.name)}
+          className="p-2 rounded-md bg-gray-300">
+          Add to Cart
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ full-stack-practice/server/server.ts
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import pg from 'pg';
+import express from 'express';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+type Products = {
+  productId: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  shortDescription: string;
+  longDescription: string;
+};
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/products', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+        from "products";
+    `;
+    const result = await db.query<Products[]>(sql);
+    if (!result) res.sendStatus(500);
+    res.json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/products/:productId', async (req, res, next) => {
+  try {
+    const productId = Number(req.params.productId);
+    if (!Number.isInteger(+productId))
+      throw new ClientError(400, `productId ${productId} must be a number.`);
+
+    const sql = `
+      select *
+        from "products"
+        where "productId" = $1
+    `;
+
+    const params = [productId];
+    const result = await db.query<Products>(sql, params);
+    if (!result) res.sendStatus(500);
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(
+  process.env.PORT,
+  () => `Express server running on port: ${process.env.PORT}`
+);
